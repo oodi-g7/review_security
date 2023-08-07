2a7ed000163f9727945d81638a5b16ae6bee7f8b
oodi-g7 <oodig7@gmail.com>
2023-08-07 23:20:07 +0900

    add loginHandler

--- src/main/java/com/study/review_security/config/SecurityConfig.java
+++ src/main/java/com/study/review_security/config/SecurityConfig.java
@@ -5,10 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration // 설정정보임을 명시
 @EnableWebSecurity // 해당 시큐리티 필터 설정값들을 기본 필터 체인에 등록
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // 생성자 주입
+    private final AuthenticationSuccessHandler loginSuccessHandler;
+    private final AuthenticationFailureHandler loginFailureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -22,8 +31,10 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login")
+                .successHandler(loginSuccessHandler)
+                .failureHandler(loginFailureHandler)
                 .usernameParameter("id")
-                .defaultSuccessUrl("/loginSuccess");
+                .defaultSuccessUrl("/loginSuccess"); // [주의] defaultUrl을 지정해주면 로그인 성공시, 로그인 성공 핸들러를 타지 않음
 
         return http.build();
     }
